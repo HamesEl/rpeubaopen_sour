ad1d96408bd0c0e9e15516ab4b65602faf64fae5
Harold Elías <129306594+HamesEl@users.noreply.github.com>
2024-11-01 16:25:29 -0500

    Add files via upload

---
+++ src/test/java/com/skillswap/platform/tutormatch/TutorMatchApplicationTests.java
@@ -0,0 +1,29 @@
+package com.skillswap.platform.tutormatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootTest
+class TutorMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
+}
